a5d9615c0a82bda5c0fb7d3bcd8335388fe3fab3
AZaidan DEV <abbas.zaidan08@gmail.com>
2024-05-14 11:03:44 +0200

    Add files via upload

---
+++ src/data/portfolio.js
@@ -0,0 +1,26 @@
+export default [
+    {
+       title: '0wos2',
+       imgUrl: '/assets/terminal-website.jpg',
+       stack: ['HTML', 'CSS', 'JavaScript','PHP'],
+       link: 'https://0wos2.azaidan.com',
+    },
+    {
+       title: 'Developer Portfolio',
+       imgUrl: '/assets/dev-portfolio.jpg',
+       stack: ['React', 'Tailwind', 'Vite'],
+       link: 'https://thehackershideout.com',
+    },
+    {
+       title: 'AI Nerf Gun',
+       imgUrl: '/assets/ai-nerf-gun.jpg',
+       stack: ['Python', 'C++', 'Robotics'],
+       link: 'https://youtu.be/A8eegxL82PI',
+    },
+    {
+      title: 'AZAIDAN',
+      imgUrl: '/assets/az.png',
+      stack: ['HTML', 'CSS', 'PHP','JavaScript'],
+      link: 'https://azaidan.com',
+   },
+ ]
---
+++ src/data/timeline.js
@@ -0,0 +1,23 @@
+export default [
+    {
+         year: 'Today',
+         title: 'Freelance Developer',
+         duration: '6 years',
+         details:
+             "I'm a solo indie developer building software for myself and clients. All coding projects are built from the ground up, from planning and designing all the way to solving real-life problems with code.",
+     },
+     {
+         year: '2019',
+         title: 'Software Developer',
+         duration: '1 year',
+         details:
+             'I helped build an enterprise application for a company in an Agile team setting. Our tech stack consisted of JavaScript, HTML, CSS, PHP, and a handful of other languages and tools.',
+     },
+     {
+         year: '2019',
+         title: 'First Line of Code',
+         duration: 'the beginning',
+         details:
+             "I wrote my first line of real code that started this journey into digital craftsmanship that I never could've imagined, especially as someone who never saw themselves as anything other than a blue collar worker.",
+     },
+ ]
